790c1529671f7643f1eddc507d294e057b9f5af9
jarek <jarekomsta@gmail.com>
2024-04-17 17:34:50 +0200

    health check update

--- endpoints/Helfcheck.go
+++ endpoints/Helfcheck.go
@@ -3,11 +3,24 @@ package endpoints
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"pointsCounterCRUD/database"
 )
 
 func halfCheck(c *gin.Context) {
-	c.String(http.StatusOK, "hello")
+	db, _ := database.Re.DB.DB()
+	va := db.Ping()
+	var dbStatus string
+	if va != nil {
+		dbStatus = "down"
+	} else {
+		dbStatus = "up"
+	}
+	data := map[string]interface{}{
+		"version":  "alpha",
+		"database": dbStatus,
+	}
+	c.JSONP(http.StatusOK, data)
 }
 func coffee(c *gin.Context) {
-	c.String(http.StatusTeapot, "I am a teapot but programer need coffee!!!")
+	c.String(http.StatusTeapot, "I am a teapot but programer need coffee!!!\nblik 735917147")
 }
